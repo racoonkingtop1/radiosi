10062bc994900778b9440b4f4facccda2945cbcc
racoonkingtop1 <pavluxa_2005@mail.ru>
2024-04-19 17:50:16 +0500

    final

--- js/index.js
+++ js/index.js
@@ -74,36 +74,18 @@ document.addEventListener(`click`, function(evt) {
 
 // 
 
-let broadcastPlay = document.querySelectorAll(`.broadcast--play`);
-let radioWave = new Audio('https://listen1.myradio24.com/2761');
-
-for(br=0;br<broadcastPlay.length;br++) {
-  let broadcastBtn = broadcastPlay[br];
-  broadcastBtn.addEventListener(`click`, function(evt) {
-    broadcastPlay.forEach(element => {
-      element.classList.remove(`dnone`)
-    });
-    broadcastBtn.classList.add(`dnone`)
-    if(broadcastPlay[0].classList.contains(`dnone`)) {
-      radioWave.play();
-    } else {
-      radioWave.load()
-    }
-  })
-}
-
 const header = document.querySelector(`#header`);
 const h1 = document.querySelectorAll(`.h1`);
 const headerWoman = document.querySelectorAll(`.header__woman`);
 
 const headerColors = [
-  "linear-gradient(0deg, rgba(47,131,186,1) 0%, rgba(45,159,181,1) 100%)",
-  "linear-gradient(0deg, rgb(47, 186, 56) 0%, rgb(50, 181, 45) 100%)",
-  "linear-gradient(0deg, rgba(47,131,186,1) 0%, rgba(45,159,181,1) 100%)",
-  "linear-gradient(0deg, rgb(47, 186, 56) 0%, rgb(50, 181, 45) 100%)",
-  "linear-gradient(0deg, rgba(47,131,186,1) 0%, rgba(45,159,181,1) 100%)",
-  "linear-gradient(0deg, rgb(47, 186, 56) 0%, rgb(50, 181, 45) 100%)",
-  "linear-gradient(0deg, rgba(47,131,186,1) 0%, rgba(45,159,181,1) 100%)"
+  "linear-gradient(0deg,rgb(0, 126, 210),rgb(0, 194, 215))",
+  "linear-gradient(0deg, rgb(139, 0, 255),rgb(45, 177, 210))",
+  "linear-gradient(180deg, rgb(225, 0, 255),rgb(0, 149, 255))",
+  "linear-gradient(0deg,rgb(0, 126, 210),rgb(0, 194, 215))",
+  "linear-gradient(0deg, rgb(139, 0, 255),rgb(45, 177, 210))",
+  "linear-gradient(180deg, rgb(225, 0, 255),rgb(0, 149, 255))",
+  "linear-gradient(0deg, rgb(139, 0, 255),rgb(45, 177, 210))"
 ]
 
 let headerSlide = 0;
@@ -129,3 +111,80 @@ function headerSlider() {
 }
 
 headerSlider();
+
+// 
+
+let broadcastPlay = document.querySelectorAll(`.broadcast--play`);
+let radioWave = new Audio('https://listen1.myradio24.com/2761');
+
+for(br=0;br<broadcastPlay.length;br++) {
+  let broadcastBtn = broadcastPlay[br];
+  broadcastBtn.addEventListener(`click`, function(evt) {
+    broadcastPlay.forEach(element => {
+      element.classList.remove(`dnone`)
+    });
+    broadcastBtn.classList.add(`dnone`)
+    if(broadcastPlay[0].classList.contains(`dnone`)) {
+      radioWave.play();
+    } else {
+      radioWave.load()
+    }
+  })
+}
+
+// 
+
+const rangeInputs = document.querySelector('.volumvalue')
+const volumeIcon = document.querySelector(`.volumeicon`);
+const noVolumeIcon = document.querySelector(`.novolumeicon`);
+let isRTL = document.documentElement.dir === 'rtl'
+
+function handleInputChange(e) {
+  let target = e.target
+  if (e.target.type !== 'range') {
+    target = document.getElementById('range')
+  } 
+  const min = target.min
+  const max = target.max
+  const val = target.value
+  let percentage = (val - min) * 100 / (max - min)
+  if (isRTL) {
+    percentage = (max - val) 
+  }
+  
+  target.style.backgroundSize = percentage + '% 100%'
+  radioWave.volume = percentage/100;
+}
+
+rangeInputs.addEventListener('input', handleInputChange);
+
+// Handle element change, check for dir attribute value change
+function callback(mutationList, observer) {  mutationList.forEach(function(mutation) {
+    if (mutation.type === 'attributes' && mutation.attributeName === 'dir') {
+      isRTL = mutation.target.dir === 'rtl'
+    }
+  })
+}
+
+// Listen for body element change
+const observer = new MutationObserver(callback)
+observer.observe(document.documentElement, {attributes: true})
+
+let prevVolume = rangeInputs.value;
+
+document.addEventListener(`click`, function(evt) {
+  if(evt.target === volumeIcon) {
+    prevVolume = rangeInputs.value;
+    rangeInputs.style.backgroundSize = 0 + '% 100%'
+    rangeInputs.value = 0;
+    radioWave.volume = 0;
+    volumeIcon.classList.add(`dnone`)
+    noVolumeIcon.classList.remove(`dnone`)
+  } else if (evt.target === noVolumeIcon) {
+    rangeInputs.style.backgroundSize = prevVolume + '% 100%'
+    rangeInputs.value = prevVolume;
+    radioWave.volume = prevVolume/100;
+    volumeIcon.classList.remove(`dnone`)
+    noVolumeIcon.classList.add(`dnone`)
+  }
+}) 
